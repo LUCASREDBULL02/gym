870783d6d9bbbcc2051d566c167a7205842963ce
LUCASREDBULL02 <lucas.alm@hotmail.se>
2026-01-01 15:01:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -544,11 +544,11 @@ export default function App() {
   return saved
     ? JSON.parse(saved)
     : {
-        name: "",
-        age: "",
-        height: "",
-        weight: "",
-        gender: "", // 👈 NYTT
+        name: "Maria Kristina",
+        age: "21",
+        height: "170",
+        weight: "68",
+        gender: "Kvinna", // 👈 NYTT
       };
 });
 
